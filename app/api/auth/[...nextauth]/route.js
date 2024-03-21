e779ab801533606551dcc84f827972932308542f
Crisppu <criyattojin@gmail.com>
2024-03-21 00:00:07 -0600

    se agg el boton sign in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,18 @@
 import NextAuth from "next-auth"
 import GoogleProvider from 'next-auth/providers/google';
 
-export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
-}
+const handler = NextAuth({
+    // Configure one or more authentication providers
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+      // ...add more providers here
+    ],
+});
 
-export default NextAuth(authOptions)
+// export default NextAuth(authOptions)
+export {handler as GET, handler as POST}
+
+//esto me permite conectarme a los servicios de Google
--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,13 +1,21 @@
 'use client'
+import { useSelector } from "react-redux";
+import { selectDarkMode } from "@/redux/features/darkModeSlice";
+import {signIn} from 'next-auth/react';
 import ButtonDarkMode from "@/components/ui/buttonDarkMode";
 
 export default function NavBar() {
+    const modeSelector = useSelector(selectDarkMode);
+
     return (
-        <div className="flex justify-end  h-20 pr-2 shrink-0 items-center  bg-green-400  md:h-[100px]">
-            <div className="flex justify-center items-center p-1">
-                <span className="text-xl">Enrique</span>
+        <div className={`flex justify-end items-center  h-20 pr-2 shrink-0  bg-green-400  md:h-[100px] ${modeSelector}`}>
+            <div className="flex items-center space-x-1">
+                <button onClick={()=>signIn()}
+                    className="dark:bg-slate-600 bg-gray-200 border-2 dark:border-slate-700 border-gray-300 rounded-lg dark:text-white text-black px-6 py-3 text-base dark:hover:border-white hover:border-black cursor-pointer transition">
+                    Sign In
+                </button>
+                <ButtonDarkMode></ButtonDarkMode>
             </div>
-            <ButtonDarkMode></ButtonDarkMode>
         </div>
     )
 }
