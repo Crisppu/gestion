47d96a1eb5c4056711669eb3a01424b96e032ee1
Crisppu <criyattojin@gmail.com>
2024-04-02 15:02:56 -0600

    el msm de error de persist en redux ya esta solucionado

--- redux/store.js
+++ redux/store.js
@@ -3,9 +3,28 @@ import counterReducer from './features/counterSlice';
 import darkModeSlice from "./features/darkModeSlice";
 import arrowSideNavSlice from "./features/arrowSideNavSlice";
 
-import storage from "redux-persist/lib/storage";
-import { persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+import { persistReducer, FLUSH, REHYDRATE,PAUSE, PERSIST, PURGE, REGISTER} from "redux-persist";
+
 import { thunk } from "redux-thunk";
+
+const createNoopStore = () =>{
+    return {
+        getItem(_key){
+            return Promise.resolve(null);
+        },
+        setItem(_key, value){
+            return Promise.resolve(value);
+        },
+        removeItem(_key){
+            return Promise.resolve();
+        },
+    };
+};
+
+const storage = typeof window !== 'undefined' ? createWebStorage('local') : createNoopStore();
 
 
 const persistConfig ={
@@ -22,12 +41,20 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
     reducer:persistedReducer,
-    middleware: (getDefaultMiddleware) =>
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(thunk),
+})
+
+/**
+ * middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
                 serializableCheck: {
                 ignoredActions: ['persist/PERSIST'],
                 ignoredActionPaths: ['register', 'rehydrate'],
             },
     }).concat(thunk)
-})
-
+ */
