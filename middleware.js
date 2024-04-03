6a8289750533337368f5c9b139a1938fafba8ac0
Crisppu <criyattojin@gmail.com>
2024-04-03 17:41:09 -0600

    protegiendo las subCarpetas

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard",'/otrasRutasmas'] }
+// export const config = { matcher: ["/dashboard",'/otrasRutasmas'] } //si solo querremos proteger una ruta especifica
+export const config = { matcher: ["/dashboard/:path*",'/otrasRutasmas/:path*'] } //todas las sub carpetas estaran protegidas
