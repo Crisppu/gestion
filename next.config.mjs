67c605473256faff25d7fefc34263634b5984459
Crisppu <criyattojin@gmail.com>
2024-04-06 18:25:05 -0600

    next.config.mjs quite la configuracion para quitar las advertencias que me sale a la hora de hacer uso de <img>

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    //esta regla me permite que las imagenes que venga de esta url esten listas antes de mostrarlas
-    images: {
-        domains: ['lh3.googleusercontent.com'],
-    },
-};
 
-
-export default nextConfig;
 
