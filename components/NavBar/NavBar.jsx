25302c1a95e07e45cacf7fbb31a3688397eb3df1
Crisppu <criyattojin@gmail.com>
2024-04-10 17:33:48 -0600

    se agg icono a la pestaña y se modifico el archivo del naBar para no tener codigo repetetivo

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -6,27 +6,26 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function NavBar() {
 
-    const session = await getServerSession(authOptions)
+    const session = await getServerSession(authOptions);
 
     return (
         <div className={`flex justify-end items-center h-full pr-2 shrink-0`} >
-            <div className="flex items-center space-x-1">
-                <ul className="flex gap-x-2">
+            <div className="flex items-center space-x-4">
+                <ul className="flex gap-x-4">
+                    <li className="hover:overline decoration-4 font-medium"><Link href={'/'}>Home</Link></li>
                     { !session?.user ?
-                    (
-                        <>
-                            <li><Link href={'/'}>Home</Link></li>
-                            <li><Link href={'/auth/register'}>Register</Link></li>
-                            <li><Link href={'/auth/login'}>Login</Link></li>
-                        </>
-                    ):
-                    (
-                        <>
-                            <li><Link href={'/'}>Home</Link></li>
-                            <li><Link href={'/dashboard'}>Dashboard</Link></li>
-                            <li><Link href={'/auth/logout'}>Logout</Link></li>
-                        </>
-                    )
+                        (
+                            <>
+                                <li className="hover:overline decoration-4 font-medium"><Link href={'/auth/register'}>Register</Link></li>
+                                <li className="hover:overline decoration-4 font-medium"><Link href={'/auth/login'}>Login</Link></li>
+                            </>
+                        ):
+                        (
+                            <>
+                                <li className="hover:overline decoration-4 font-medium"><Link href={'/dashboard'}>Dashboard</Link></li>
+                                <li className="hover:overline decoration-4 font-medium"><Link href={'/auth/logout'}>Logout</Link></li>
+                            </>
+                        )
 
                     }
                 </ul>
