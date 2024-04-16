15541c07106fa12fc74c1163a3ffc30ae2eeea08
Crisppu <criyattojin@gmail.com>
2024-04-15 23:48:39 -0600

    se le dio estilos la pagina de logout-cerrar session next auth

--- app/auth/logout/page.jsx
+++ app/auth/logout/page.jsx
@@ -7,13 +7,12 @@ import Link from "next/link";
 export default function Page() {
   const modeSelector = useSelector(selectDarkMode);
     return (
-        <div className={`${modeSelector} dark:bg-black h-screen flex items-center justify-center flex-col`}>
+        <div className={`${modeSelector} dark:bg-black h-screen flex items-center justify-center flex-col gap-2`}>
             <Link href={'/'}><img src='/logoSantaAna.png' alt='logo Santa Ana' className={'w-40 h-24'}></img></Link>
-            <p>¿Quieres cerrar session?</p>
+            <p className='text-black dark:text-white text-xl'>¿Quieres cerrar session?</p>
             <button onClick={ () => {signOut({callbackUrl:'/'})}} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-lg hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80">
                 logout
             </button>
-
         </div>
     )
 }
