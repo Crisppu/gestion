d33a0a02aaff7d5e3d610d4e30ea3606792f10b2
Crisppu <criyattojin@gmail.com>
2024-04-17 22:27:39 -0600

    se mejoro el tamaño del icono de la pagina logout

--- app/auth/logout/page.jsx
+++ app/auth/logout/page.jsx
@@ -8,7 +8,7 @@ export default function Page() {
   const modeSelector = useSelector(selectDarkMode);
     return (
         <div className={`${modeSelector} dark:bg-black h-screen flex items-center justify-center flex-col gap-2`}>
-            <Link href={'/'}><img src='/logoSantaAna.png' alt='logo Santa Ana' className={'w-40 h-24'}></img></Link>
+            <Link href={'/'}><img src='/logoSantaAna.png' alt='logo Santa Ana' width={60} height={60}></img></Link>
             <p className='text-black dark:text-white text-xl'>¿Quieres cerrar session?</p>
             <button onClick={ () => {signOut({callbackUrl:'/'})}} className="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-red-600 rounded-lg hover:bg-red-500 focus:outline-none focus:ring focus:ring-red-300 focus:ring-opacity-80">
                 logout
