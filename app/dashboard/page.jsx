f4648d99f8885a25f9143ec807e400084721a23c
Crisppu <criyattojin@gmail.com>
2024-07-21 19:50:53 -0600

    configuracion de next UI . realizado con exito

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,12 @@
+import {Button} from '@nextui-org/react'
 
 export default function Page() {
-  return (
-    <div className={'text-green-600 '}>page dashboard</div>
-  )
+    return (
+        <div className={'text-green-600 '}>
+            <Button>Button de next UI</Button>
+            <Button color="primary">
+             Button de next UI
+            </Button>
+        </div>
+    )
 }
--- redux/providers.jsx
+++ redux/providers.jsx
@@ -4,17 +4,21 @@ import { store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 import { SessionProvider } from 'next-auth/react';
+import {NextUIProvider} from '@nextui-org/react'
+
 
 var persistor = persistStore(store);
 export function Providers({children}) {
     return (
-        <Provider store={store}>
-            <PersistGate persistor={persistor}>
-                <SessionProvider>
-                    {children}
-                </SessionProvider>
-            </PersistGate>
-        </Provider>
+        <NextUIProvider>
+            <Provider store={store}>
+                <PersistGate persistor={persistor}>
+                    <SessionProvider>
+                        {children}
+                    </SessionProvider>
+                </PersistGate>
+            </Provider>
+        </NextUIProvider>
     )
 }
 
