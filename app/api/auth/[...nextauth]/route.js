9ff32aaff568f3339f233418cc2043d4c42c17be
Crisppu <criyattojin@gmail.com>
2024-08-06 17:09:50 -0600

    fecha de expiracion de session congiurado a 24 horas

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -21,8 +21,9 @@ export const authOptions = {
                         const matchPassword = await bcrypt.compare(credentials.password,  response.data.contrasenia);
                         if(!matchPassword)  throw new Error('Wrong password')
                         return{
-                            id:response.data.id,
-                            email:response.data.correo
+                            name:response.data.id,
+                            email:response.data.correo,
+                            image:null //pendiente de agregar imagen.
                         }
 
                     }catch(error){
@@ -36,7 +37,31 @@ export const authOptions = {
     pages:{
         signIn:'/auth/login',
         signOut:'/auth/logout'
-    }
+    },
+    //configuracion de expiracion de sesion, esta coonfigurado a 24 horas despues se cierra la sesion.
+    /**ejemplo para verificar la fecha de expiracion
+     * import { useSession } from 'next-auth/react';
+     * const session = useSession();
+     *console.log(new Date(session.data.expires).toLocaleString())
+    */
+    session: {
+        strategy: 'jwt',
+        maxAge: 24 * 60 * 60, // 1 day in seconds
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.user = user;
+                token.expires = Math.floor(Date.now() / 1000) + 24 * 60 * 60; // 1 day in seconds
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.user = token.user;
+            session.expires = new Date(token.expires * 1000).toISOString();
+            return session;
+        },
+    },
 }
 
 const handler = NextAuth(authOptions);
